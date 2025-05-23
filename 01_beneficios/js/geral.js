5516e1a3b5f32d3e1788e5720943c049c762151f
Paulo-SenacRS <prjunior@senacrs.com.br>
2025-05-23 14:16:47 -0300

    diagramação uc 05

---
+++ 01_beneficios/js/geral.js
@@ -0,0 +1,6 @@
+$(".btn-organizacao").click(function(){
+    $(".check-organizacao").css('display','block');
+});
+$(".btn-funcionario").click(function(){
+    $(".check-funcionario").css('display','block');
+});
