4fffd85843cff7abcba8c329105c16fc6b49dd8c
Paulo-SenacRS <prjunior@senacrs.com.br>
2025-05-26 04:53:54 -0300

    diagramação uc 05

---
+++ 02_beneficios_legais/js/geral.js
@@ -0,0 +1,47 @@
+$(".btn-1848").click(function(){
+    $(this).css('background-image','url(img/painel01/bt01_hover.png)');
+});
+$(".btn-1872").click(function(){
+    $(this).css('background-image','url(img/painel01/bt02_hover.png)');
+});
+$(".btn-1925").click(function(){
+    $(this).css('background-image','url(img/painel01/bt03_hover.png)');
+});
+$(".btn-1934").click(function(){
+    $(this).css('background-image','url(img/painel01/bt04_hover.png)');
+});
+$(".btn-1943").click(function(){
+    $(this).css('background-image','url(img/painel01/bt05_hover.png)');
+});
+$(".btn-1988").click(function(){
+    $(this).css('background-image','url(img/painel01/bt06_hover.png)');
+});
+
+$(".btn-1848_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt01_hover.png)');
+});
+$(".btn-1872_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt02_hover.png)');
+});
+$(".btn-1925_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt03_hover.png)');
+});
+$(".btn-1934_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt04_hover.png)');
+});
+$(".btn-1943_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt05_hover.png)');
+});
+$(".btn-1988_mobile").click(function(){
+    $(this).css('background-image','url(img/painel01/bt06_hover.png)');
+});
+
+$(".btn-aquisitivo").click(function(){
+    $(".check-aquisitivo").css('display','block');
+});
+$(".btn-concessivo").click(function(){
+    $(".check-concessivo").css('display','block');
+});
+$(".btn-parcelamento").click(function(){
+    $(".check-parcelamento").css('display','block');
+});
